5041493054c611e164e80ba6c8808e8fd8fc4a6b
michellenesbitt <mn218@nerdymail.com>
2020-07-05 14:05:23 -0400

    mn

---
+++ nesbitt-codeattrib.js
@@ -0,0 +1,22 @@
+/*
+======================================================
+; Title:  nesbitt-assignment
+; Author: Richard Krasso
+; Date:   2020
+; Modified By: Michelle Nesbitt
+; Description: Demonstrates
+;=====================================================
+*/
+var assignment = "x.x"
+//Print my header to the screen
+const header = require('../Nesbitt-header.js');
+//Display my header with exercise number
+console.log(header.display("Michelle", "Nesbitt", "Exercise " + assignment));
+
+/*
+  Expected output:
+
+  Michelle Nesbitt
+  Exercise Number
+  Today's Date
+  */
