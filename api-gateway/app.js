0d82da35b4ab584c7fa8f3312d455359c55514b9
Michelle Nesbitt <mn218@nerdymail.com>
2020-07-11 08:49:26 -0400

    mn

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -37,7 +37,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-App.use('/api', apiCatalog); 
+app.use('/api', apiCatalog); 
 app.use('/', indexRouter);
 
 
